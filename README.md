f75bfa7b560cd6eee10f8a407b4f6faab5b55419
蘇健豪 <jenhausu@osensetech.com>
2022-10-24 14:21:33 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ViewModifierLibrary
+
+A description of this package.
---
+++ Sources/ViewModifierLibrary/ViewModifierLibrary.swift
@@ -0,0 +1,6 @@
+public struct ViewModifierLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
